e23e5001ffdec085c7ca70f93368e6ca74457857
Atharva <chilwerwaratharva@gmail.com>
2025-02-06 07:16:53 +0530

    Initial Commit

---
+++ src/Main.kt
@@ -0,0 +1,13 @@
+fun main() {
+    println("Hello World!")
+
+    //Printing a variable
+    var myVariable : Int = 5
+    println("This is the value of my variable: $myVariable")
+
+    //Performing arithmetic operations
+    var a = 10
+    var b = 8
+    a += a
+    println(a+b)
+}

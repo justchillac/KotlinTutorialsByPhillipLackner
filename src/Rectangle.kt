5f7eb5bb07f2ab10ff02c5831793edd902738e78
Atharva <chilwerwaratharva@gmail.com>
2025-02-10 12:09:58 +0530

    Included area and perimeter functions for Rectangle class

--- src/Rectangle.kt
+++ src/Rectangle.kt
@@ -5,4 +5,10 @@ class Rectangle(
     init {
         println("Rectangle created with side $a and $b")
     }
+
+    fun area() = a*b
+
+    fun perimeter() = 2 * (a+b)
+
+    fun isSquare() = a==b
 }
